947b715c8df3abe18651c0e2a1b2b2987f6f1c63
NaghamAmr <naghamamr@gmail.com>
2018-04-28 18:30:35 +0200

    level one and puzzle made by yassin and radwan , nada made level 2 and third person character , nagham level 3 and UI ,, still AI and health

---
+++ Assets/nada/Script/MovementController.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+
+public class MovementController : MonoBehaviour {
+
+	public bool isGrounded;
+	private float speed;
+	public float rotSpeed;
+	public float jumpHeight;
+	//walk speed
+	private float w_speed = 0.05f;
+	//rotation speed
+	private float rot_speed = 1.0f;
+	Rigidbody rb;
+	Animator anim;
+
+	//startNG
+
+
+	//END
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+		isGrounded = true; //indicate that we are in the ground
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (isGrounded) {
+			//moving forward and backward
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				speed = w_speed;
+				movementControl ("WalkingForward");
+			   }
+			      else if (Input.GetKey(KeyCode.DownArrow))
+			      {
+				    speed = w_speed;
+				    movementControl("WalkingBackward");
+			      }
+			else if (Input.GetKey (KeyCode.LeftShift)) {
+				speed = 2f;
+				movementControl("Running");
+			     } 
+			      else
+			      {
+			    	movementControl("idle");
+			      }
+			//moving right and left
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				rotSpeed = rot_speed;
+			}
+				
+			    else if (Input.GetKey(KeyCode.RightArrow))
+			    {
+				  rotSpeed = rot_speed;
+			    }
+			    else
+			    {
+				  rotSpeed = 0;
+			    }
+			var z = Input.GetAxis("Vertical") * speed;
+			var y = Input.GetAxis("Horizontal") * rotSpeed;
+			transform.Translate(0, 0, z);
+			transform.Rotate(0, y, 0);
+
+			if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+			{
+				anim.SetTrigger("isJumping");
+				rb.AddForce(0, jumpHeight * Time.deltaTime, 0);
+
+			}
+
+
+
+		}
+		}
+
+		//jumping function
+
+
+
+	void movementControl(string state)
+	{
+		switch (state)
+		{
+		case "WalkingForward":
+			anim.SetBool("isWalkingForward", true);
+			anim.SetBool("isWalkingBackward", false);
+			anim.SetBool("isIdle", false);
+			anim.SetBool ("isRunning", false);
+			break;
+		case "WalkingBackward":
+			anim.SetBool("isWalkingForward", false);
+			anim.SetBool("isWalkingBackward", true);
+			anim.SetBool("isIdle", false);
+			anim.SetBool ("isRunning", false);
+			break;
+		case "idle":
+			anim.SetBool("isWalkingForward", false);
+			anim.SetBool("isWalkingBackward", false);
+			anim.SetBool("isIdle", true);
+			anim.SetBool ("isRunning", false);
+			break;
+		case "Jumping":
+			anim.SetBool ("isWalkingForward", false);
+			anim.SetBool ("isWalkingBackward", false);
+			anim.SetBool ("isIdle", false);
+			anim.SetTrigger ("isJumping");
+			anim.SetBool ("isRunning", false);
+			break;
+		case "Running":
+			anim.SetBool ("isWalkingForward", false);
+			anim.SetBool ("isWalkingBackward", false);
+			anim.SetBool ("isIdle", false);
+			anim.SetBool ("isRunning", true);
+			break;
+		}
+	}
+}
+
---
+++ Assets/nada/Script/Respawn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	[SerializeField] private Transform player;
+	[SerializeField] private Transform respwanPoint;
+
+	void OnTriggerEnter(Collider other){
+
+		player.transform.position = respwanPoint.transform.position;
+}
+}
